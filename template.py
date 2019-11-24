4f21508f2a905ba9efa5f6bdc7e8894b277dc3d8
noteisenheim <39269548+noteisenheim@users.noreply.github.com>
2019-11-25 02:42:53 +0300

    added template for sql queries

---
+++ template.py
@@ -0,0 +1,41 @@
+def connect(host, database, user, password):
+	'''
+	arguments: all strings, hostname, database name, user name, password
+
+	establishes connection with the database
+
+	returns nothing (may throw an exception if unable to esablish connection)
+	'''
+
+def query_1(id, connection):
+	'''
+	arguments: id - integer patient_id, connection to database
+
+	returns array of tuples in a form [(doctor_name1), (doctor_name2), ...]
+	'''
+
+def query_2(connection):	
+	'''
+	HAVE NO IDEA WHAT RETURNED THINGY WILL LOOK LIKE
+	'''
+
+def query_3(connection):
+	'''
+	arguments: connection to database
+
+	returns array of tuples in a form [(patient_id1, patient_name1), (patient_id1, patient_name1), ...]
+	'''
+
+def query_4(connection):
+	'''
+	arguments: connection to database
+
+	returns array of one tuple in form [(income)]
+	'''
+
+def query_5(connection):
+	'''
+	arguments: connection to database
+
+	returns array of tuples in a form [(doctor_id1, doctor_name1), (doctor_id2, doctor_name2), ...]
+	'''
