251c8eba1afe57239a9d496ce587bd414ce4f90b
DmitriyPodpryatov <podp2000@gmail.com>
2019-11-26 16:18:31 +0300

    Added MySql population

--- population/Source/video_record.py
+++ population/Source/video_record.py
@@ -9,3 +9,9 @@ with open('hospital.sql', 'a+') as f:
         camera_number = fake.random_int(min=1, max=50)
         date = fake.date_this_month(before_today=True, after_today=False)
         print("INSERT INTO Video_Record(Date, Path, Camera_number) VALUES(" + wrap(date) + " , '/dev/hospital/video_records/" + str(date) + "/" + fake.file_name(extension="avi") + "' , " + str(camera_number) + " );", file=f)
+
+with open('hospital_mysql.sql', 'a+') as f:
+    for i in range(n):
+        camera_number = fake.random_int(min=1, max=50)
+        date = fake.date_this_month(before_today=True, after_today=False)
+        print("INSERT INTO Video_Record(Date, Path, Camera_number) VALUES(" + wrap(date) + " , '/dev/hospital/video_records/" + str(date) + "/" + fake.file_name(extension="avi") + "' , " + str(camera_number) + " );", file=f)
